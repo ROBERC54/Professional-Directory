bfa86d9875a5fc4eb7d8318f1b204cfb94d5ae33
Christian Roberts <christian.m.roberts@outlook.com>
2020-04-06 09:54:22 -0400

    set up the database

---
+++ ProfessDirectData/ProfessDirectData/Files.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Files] (
+    [EnrollmentID] INT IDENTITY (1, 1) NOT NULL,
+    [Ranking]        DECIMAL(3, 2) NULL,
+    [IndustryID]     INT NOT NULL,
+    [ContactID]    INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([EnrollmentID] ASC),
+    CONSTRAINT [FK_dbo.Files_dbo.Industry_IndustryID] FOREIGN KEY ([IndustryID]) 
+        REFERENCES [dbo].[Industry] ([IndustryID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Files_dbo.Contact_ContactID] FOREIGN KEY ([ContactID]) 
+        REFERENCES [dbo].[Contact] ([ContactID]) ON DELETE CASCADE
+)
