bf589c83539af9838070e528244d18e251832f37
Christian Roberts <christian.m.roberts@outlook.com>
2020-04-12 23:56:00 -0400

    still working on refactor

---
+++ ProfessDirectData/ProfessDirectSite/Controllers/FilesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProfessDirectSite.Models;
+
+namespace ProfessDirectSite.Controllers
+{
+    public class FilesController : Controller
+    {
+        private ProfessDirectDataEntities db = new ProfessDirectDataEntities();
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            var files = db.Files.Include(f => f.Contact).Include(f => f.Industry);
+            return View(files.ToList());
+        }
+
+        // GET: Files/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: Files/Create
+        public ActionResult Create()
+        {
+            ViewBag.ContactID = new SelectList(db.Contacts, "ContactID", "LastName");
+            ViewBag.IndustryID = new SelectList(db.Industries, "IndustryID", "Title");
+            return View();
+        }
+
+        // POST: Files/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentID,Ranking,IndustryID,ContactID")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ContactID = new SelectList(db.Contacts, "ContactID", "LastName", file.ContactID);
+            ViewBag.IndustryID = new SelectList(db.Industries, "IndustryID", "Title", file.IndustryID);
+            return View(file);
+        }
+
+        // GET: Files/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ContactID = new SelectList(db.Contacts, "ContactID", "LastName", file.ContactID);
+            ViewBag.IndustryID = new SelectList(db.Industries, "IndustryID", "Title", file.IndustryID);
+            return View(file);
+        }
+
+        // POST: Files/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentID,Ranking,IndustryID,ContactID")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ContactID = new SelectList(db.Contacts, "ContactID", "LastName", file.ContactID);
+            ViewBag.IndustryID = new SelectList(db.Industries, "IndustryID", "Title", file.IndustryID);
+            return View(file);
+        }
+
+        // GET: Files/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
